eaa58a4cae4cf646c4aa8279e7085c8c793db5d4
Maxwell Kimutai <maxwellkimutai@gmail.com>
2019-04-17 09:51:11 +0300

    make buttons responsive

--- js/main.js
+++ js/main.js
@@ -1,17 +1,3 @@
-// ===== Scroll to Top ====
-$(window).scroll(function() {
-    if ($(this).scrollTop() >= 50) { // If page is scrolled more than 50px
-        $('#return-to-top').fadeIn(200); // Fade in the arrow
-    } else {
-        $('#return-to-top').fadeOut(200); // Else fade out the arrow
-    }
-});
-$('#return-to-top').click(function() { // When arrow is clicked
-    $('body,html').animate({
-        scrollTop: 0 // Scroll to top of body
-    }, 500);
-});
-
 $(window)
     .resize(function() {
         if ($(window).width() <= 1024) $('.module-icons').addClass("flex-column");
